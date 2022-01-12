2f8f7d730f36e9e48c670046ea2b603cb00a3ea9
bagrodia09 <shubham.bagrodia@knoldus.com>
2022-01-13 00:25:52 +0530

    File Made

--- src/main/scala/edu/knoldus/Sorting.scala
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -1,11 +1,48 @@
 package edu.knoldus
 
+import scala.annotation.tailrec
+
 class Sorting {
 
-  def insertionSort(array: Array[Int]): Array[Int] = ???
+  def insertionSort(array: Array[Int]): Array[Int] = {
+    val myDataList: List[Int] = array.toList
+    def InsideInsertionSort(list: List[Int]): List[Int] =
+      if (list.isEmpty) Nil
+      else insert(list.head, InsideInsertionSort(list.tail))
 
-  def selectionSort(array: Array[Int]): Array[Int] = ???
+    def insert(x: Int, list: List[Int]): List[Int] =
+      if (list.isEmpty || x <= list.head) x :: list
+      else list.head :: insert(x, list.tail)
 
-  def bubbleSort(array: Array[Int]): Array[Int] = ???
+    InsideInsertionSort(myDataList).toArray
+  }
+
+  def selectionSort(array: Array[Int]): Array[Int] = {
+    @tailrec
+    def InsideSelectionSort(array: Array[Int], list: List[Int] = List()): Array[Int] = {
+      val min = array.min
+      val anOtherList = list ++ array.toList.filter(_ == min)
+      if (array.min == array.max) anOtherList.toArray
+      else InsideSelectionSort(array.filter(_ > min), anOtherList)
+    }
+    InsideSelectionSort(array)
+  }
+
+  def bubbleSort(array: Array[Int]): Array[Int] = {
+    @tailrec
+    def InsideBubbleSort(array: Array[Int], len: Int): Int = {
+      if (len == 1) return 0
+      for (i <- 0 until len - 1) {
+        if (array(i) > array(i + 1)) {
+          val temp = array(i)
+          array(i) = array(i + 1)
+          array(i + 1) = temp
+        }
+      }
+      InsideBubbleSort(array, len - 1)
+    }
+    InsideBubbleSort(array, array.length)
+    array
+  }
 
 }
